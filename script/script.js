069cf0847c3f2eb8676adf1ade6a9ebd41b27b16
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-16 15:35:18 +0200

    fix problem and remove function

--- script/script.js
+++ script/script.js
@@ -7,15 +7,13 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        generate10RandomEmails() {
-            for (let i = 0; i <= 10; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.randomEmails.push(serverResponse.data.response));
-            }
-        }
+        
     },
 
     created() {
-        
+        for (let i = 0; i <= 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.randomEmails.push(serverResponse.data.response));
+            }
     }
 });
 
