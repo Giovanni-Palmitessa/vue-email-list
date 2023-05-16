988cc79b95adf03adb9702635c56560d4dd23de5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-16 16:02:42 +0200

    add button and style for changing emails

--- script/script.js
+++ script/script.js
@@ -7,15 +7,17 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        genereteEmails() {
+        generateEmails() {
+            this.randomEmails = [];
             for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.randomEmails.push(serverResponse.data.response));
             }
+            
         }
     },
 
     created() {
-        this.genereteEmails();
+        this.generateEmails();
     }
 });
 
