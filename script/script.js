662b2b56b3af225ae3c76c6f8e44f7ede9048be7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-16 15:35:58 +0200

    adjust for cycle

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ const app = Vue.createApp({
     },
 
     created() {
-        for (let i = 0; i <= 10; i++) {
+        for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverResponse => this.randomEmails.push(serverResponse.data.response));
             }
     }
